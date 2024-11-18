96b113dd182a4aa8b74c513024816ecb737ffea5
Tobiky <12644606+Tobiky@users.noreply.github.com>
2024-11-18 09:36:15 +0100

    export lib functions

--- d10-perlin-noise/src/lib.rs
+++ d10-perlin-noise/src/lib.rs
@@ -1,8 +1,8 @@
-fn lerp(a: f32, b: f32, weight: f32) -> f32 {
+pub fn lerp(a: f32, b: f32, weight: f32) -> f32 {
     weight.clamp(0., 1.).mul_add(b - a, a)
 }
 
-fn blerp(tl: f32, tr: f32, bl: f32, br: f32, x_weight: f32, y_weight: f32) -> f32 {
+pub fn blerp(tl: f32, tr: f32, bl: f32, br: f32, x_weight: f32, y_weight: f32) -> f32 {
     let lower = lerp(bl, br, x_weight);
     let upper = lerp(tl, tr, x_weight);
     let middle = lerp(lower, upper, y_weight);
@@ -10,20 +10,20 @@ fn blerp(tl: f32, tr: f32, bl: f32, br: f32, x_weight: f32, y_weight: f32) -> f3
     middle
 }
 
-fn random_gradient(#[allow(unused_variables)] grid_point @ [x, y]: [i32; 2]) -> [f32; 2] {
+pub fn random_gradient(#[allow(unused_variables)] grid_point @ [x, y]: [i32; 2]) -> [f32; 2] {
     fastrand::seed((x as u64) << 32 | y as u64);
     let random = fastrand::f32() * 2. * std::f32::consts::PI;
     [random.cos(), random.sin()]
 }
 
-fn dot(
+pub fn dot(
     #[allow(unused_variables)] a @ [x1, y1]: [f32; 2],
     #[allow(unused_variables)] b @ [x2, y2]: [f32; 2],
 ) -> f32 {
     x1.mul_add(x2, y1 * y2)
 }
 
-fn grid_point_gradient(
+pub fn grid_point_gradient(
     #[allow(unused_variables)] grid_point @ [gx, gy]: [i32; 2],
     #[allow(unused_variables)] point @ [px, py]: [f32; 2],
 ) -> f32 {
